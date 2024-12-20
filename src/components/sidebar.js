91d7b3c76687070bad6143be0febd129bbdb2406
David <dbeltra@gmail.com>
2024-12-20 12:32:44 +0100

    fix: avoid colors being purged on production build

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -42,12 +42,9 @@ const Sidebar = () => {
     <div className="row-span-2 px-4 py-2 lg:p-4">
       {/* Top Section */}
       <div className="flex items-center">
-        {["red-600", "yellow-400", "green-600"].map((color) => (
-          <span
-            key={color}
-            className={`bg-${color} w-3 h-3 rounded-full mr-2`}
-          ></span>
-        ))}
+        <span className="bg-red-600 w-3 h-3 rounded-full mr-2"></span>
+        <span className="bg-yellow-400 w-3 h-3 rounded-full mr-2"></span>
+        <span className="bg-green-600 w-3 h-3 rounded-full mr-2"></span>
         <span>Resumé</span>
         <span className="lg:hidden pl-1">- David Beltrà</span>
       </div>
